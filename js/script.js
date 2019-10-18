1e12ae9f4a8e2666895aa27e8d866660341ee7ba
valeria.osoriou <Val3r1ta1>
2019-10-18 15:31:10 +1300

    2 ejercicios hechos

---
+++ js/script.js
@@ -0,0 +1,90 @@
+console.log('hola');
+
+/* PALINDROME
+1)	Take a string from the user as input and store it in a variable (givenString).
+2)	Reverse the given string and store it in another variable (reversedString).
+3)	Check if the above two strings are equivalent. If they are equivalent display a message “Given string is a palindrome” 
+otherwise display a message “Given strings are not palindrome. You may also display the values of both the strings givenString and reversedString. */
+
+
+// var givenString = 0;
+// var reversedString = 0;
+
+// givenString = prompt('Enter your name please'); //user input
+// 	console.log(givenString);
+// reversedString = givenString.split(''); //convert givenString into an array
+// 	console.log(reversedString);
+// reversedString = reversedString.reverse(); // reverse givenString
+// 	console.log(reversedString);
+// reversedString = reversedString.join('')// join put together the reversed values
+// 	console.log(reversedString);
+
+// //Now we write the conditions asked above in the excercise
+// if (givenString === reversedString) { //Palindrome is  word that is readable in the same order backwards so they have to be equals
+// 	document.getElementById('palindrome').textContent = 'Your name backwards is: ' + reversedString;
+// } else {
+// 	document.getElementById('palindrome').textContent = 'You can\'t make your name a palindrome, ' + givenString;
+// }
+
+
+// ODD NUMBER
+/*
+1)	Take a number from the user as input and store it in a variable (x).
+2)	Check if the given number is even number or odd number and display the relevant message to the user as output.
+	An odd number ends in 1, 3, 5, 7, or 9, so if we divide by 2 should tell us if is odd or not */
+
+var givenNumber = 0;
+
+givenNumber = prompt('Enter a number');
+	console.log(givenNumber);
+givenNumber = givenNumber.split();// string into array
+	console.log(givenNumber);{
+
+if (givenNumber%2 === 0){
+	console.log('number is even');
+} else {
+	console.log('number is not even');
+}
+}
+
+
+
+//SENTENCE TO BE STORED
+
+/*
+1)	Store the following sentence in a variable (givenSentence).
+“The good news is that everyone makes mistakes. All of the mistakes you make, have been made by others who started off just like you.”
+2)	Replace the word mistakes with bugs and store it in a variable (replacedSentence).
+3)	Output the replacedSentence.
+
+
+
+
+
+
+
+// FRUITS ALGORITHM
+
+/*
+1)	Create 4 buttons for add to, delete from, search in and sort an array.
+2)	Create a text box to read a string value from the user.
+3)	Store your favourite foods or cars or dogs in an array (you can one choose from foods, cars and dogs). The array must contain 3 elements initially. 
+
+Example: var fruits = {“Apple”, “Orange”, “Kiwi”};
+
+4)	Write reusable code for each operation mentioned in 1) to add a new element to the array, to delete an element from the array, to search a given 
+element in the array and to sort the elements in descending order and display it to the user. For all the operations provide meaningful messages whether 
+succeeded or failed, and what is the output if succeeded and reason why it failed if known. */
+
+
+
+
+
+
+
+
+
+
+
+
+
